52d6ec1943c70dd84e80ce7d71142cd5f9889d58
luowenbin <luowenbin@hey900.com>
2019-06-23 18:22:53 +0800

    心累的改动3

--- serviceprovider/src/main/java/io/demo/zuul/controller/Demo.java
+++ serviceprovider/src/main/java/io/demo/zuul/controller/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("我在feature分支上第1次改动");
         System.out.println("我在feature分支上第2次改动");
+        System.out.println("我在feature分支上第3次改动");
     }
 }
