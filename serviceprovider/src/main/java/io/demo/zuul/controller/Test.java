6d29c6a9bd503a7f0c141f3356e235cf3bc99a76
luowenbin <luowenbin@hey900.com>
2019-06-23 15:00:30 +0800

    我现在会了

---
+++ serviceprovider/src/main/java/io/demo/zuul/controller/Test.java
@@ -0,0 +1,9 @@
+package io.demo.zuul.controller;
+
+public class Test {
+    // 我在feature2上改动的记录3
+    public static void main(String[] args) {
+        System.out.println("我在feature2上改动的记录4");
+        System.out.println("我在feature2上改动的记录5");
+    }
+}
