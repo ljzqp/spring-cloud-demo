0f000ed754907c8c1c39d8a14bf0201b7259391d
luowenbin <luowenbin@hey900.com>
2019-06-23 18:22:05 +0800

    心累的改动2

--- serviceprovider/src/main/java/io/demo/zuul/controller/Demo.java
+++ serviceprovider/src/main/java/io/demo/zuul/controller/Demo.java
@@ -3,5 +3,6 @@ package io.demo.zuul.controller;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("我在feature分支上第1次改动");
+        System.out.println("我在feature分支上第2次改动");
     }
 }
